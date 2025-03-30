a4d9f0fcb5b01950a1c9d580daa50dd1aab5080a
Prof-Percival <percival.mrd921@yahoo.com>
2025-03-30 20:07:20 +0200

    add missing person domain models

---
+++ api/MCSA-API/Domain/MissingPersons/MissingPerson.cs
@@ -0,0 +1,38 @@
+﻿using MCSA_API.Domain.Moderation;
+
+namespace MCSA_API.Domain.MissingPersons;
+
+public class MissingPerson
+{
+    public int? Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string Gender { get; set; }
+    public string Race { get; set; }
+    public string Province { get; set; }
+    public DateTime DateWentMissing { get; set; }
+    public string HairColour { get; set; }
+    public string HairLengthOrStyle { get; set; }
+    public string EyeColour { get; set; }
+    public int Height { get; set; }
+    public double Weight { get; set; }
+    public string MarksTattoos { get; set; }
+    public string LastSeenWearing { get; set; }
+    public string LocationLastSeen { get; set; }
+    public string DetailsOfDisappearance { get; set; }
+    public string ImageUrl { get; set; }
+    public string PoliceStation { get; set; }
+    public string CaseNumber { get; set; }
+    public string InvestigatingOfficer { get; set; }
+    public string InvestigatingOfficerContactNumber { get; set; }
+    public string FamilyContactName { get; set; }
+    public string FamilyContactNumber { get; set; }
+    public MissingPersonStatus Status { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime Updated { get; set; }
+    public ModerationStatus ModerationStatus { get; set; }
+    public int? LastEditedByUserId { get; set; }
+
+    public bool IsNew => !Id.HasValue;
+}
