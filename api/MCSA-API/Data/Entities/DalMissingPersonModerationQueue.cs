e82771c3b924bd36bf73be085f0f7f218c359704
Prof-Percival <percival.mrd921@yahoo.com>
2025-03-30 20:05:42 +0200

    add DAL entities

---
+++ api/MCSA-API/Data/Entities/DalMissingPersonModerationQueue.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MCSA_API.Data.Entities;
+
+[Table("MissingPersonModerationQueue")]
+public class DalMissingPersonModerationQueue
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [Required]
+    public DateTime Created { get; set; }
+
+    [Required]
+    public DateTime Updated { get; set; }
+
+    public int? ModeratedByUserId { get; set; }
+
+    [Required]
+    public int ModerationStatusId { get; set; }
+
+    [MaxLength(255)]
+    public string ModerationStatusReason { get; set; }
+
+    [Required]
+    public int MissingPersonId { get; set; }
+
+    public DalUser ModeratedByUser { get; set; }
+    public DalMissingPerson MissingPerson { get; set; }
+}
---
+++ api/MCSA-API/Data/Entities/DalUser.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MCSA_API.Data.Entities;
+
+[Table("User")]
+public class DalUser
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(255)]
+    public string Username { get; set; }
+
+    [Required]
+    public string PasswordHash { get; set; }
+
+    [Required]
+    [MaxLength(150)]
+    public string FirstName { get; set; }
+
+    [Required]
+    [MaxLength(150)]
+    public string LastName { get; set; }
+
+    [Required]
+    [MaxLength(150)]
+    public string Role { get; set; }
+
+    [Required]
+    public DateTime Created { get; set; }
+
+    [Required]
+    public DateTime Updated { get; set; }
+}
