b76852977051ba3e63ac7fefb7c59c6100a6436c
Prof-Percival <percival.mrd921@yahoo.com>
2025-03-30 20:08:50 +0200

    add moderation models

---
+++ api/MCSA-API/Domain/Moderation/MissingPersonModerationQueue.cs
@@ -0,0 +1,13 @@
+﻿namespace MCSA_API.Domain.Moderation;
+
+public sealed class MissingPersonModerationQueue
+{
+    public int? Id { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime Updated { get; set; }
+    public int? ModeratedByUserId { get; set; }
+    public ModerationStatus ModerationStatusId { get; set; }
+    public string ModerationStatusReason { get; set; }
+    public int MissingPersonId { get; set; }
+    public bool IsNew => !Id.HasValue;
+}
