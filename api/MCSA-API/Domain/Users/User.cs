fa7048dbbdb45957b6512e81629b15bc622378f3
Prof-Percival <percival.mrd921@yahoo.com>
2025-03-30 20:05:56 +0200

    add domain user models

---
+++ api/MCSA-API/Domain/Users/User.cs
@@ -0,0 +1,15 @@
+﻿namespace MCSA_API.Domain.Users;
+
+public class User
+{
+    public int? Id { get; set; }
+    public string Username { get; set; }
+    public string PasswordHash { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public UserRole Role { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime Updated { get; set; }
+
+    public bool IsNew => !Id.HasValue;
+}
---
+++ api/MCSA-API/Domain/Users/UserRole.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace MCSA_API.Domain.Users;
+
+public enum UserRole
+{
+    [Description("Super User")]
+    SuperUser = 1,
+
+    [Description("Operational Support")]
+    OperationalSupport,
+
+    [Description("Receptionist")]
+    Receptionist
+}
