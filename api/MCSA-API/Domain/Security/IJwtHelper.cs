a4327cf3cb425b39802725eb75d0ee10ec5ed559
Prof-Percival <percival.mrd921@yahoo.com>
2025-03-30 21:18:58 +0200

    add JWT helper

---
+++ api/MCSA-API/Domain/Security/IJwtHelper.cs
@@ -0,0 +1,8 @@
+﻿using MCSA_API.Domain.Users;
+
+namespace MCSA_API.Domain.Security;
+
+public interface IJwtHelper
+{
+    string GenerateJwtToken(int userId, UserRole userRole);
+}
---
+++ api/MCSA-API/Domain/Security/JwtHelper.cs
@@ -0,0 +1,47 @@
+﻿using MCSA_API.Domain.Dates;
+using MCSA_API.Domain.Users;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace MCSA_API.Domain.Security;
+
+public class JwtHelper : IJwtHelper
+{
+    private readonly JwtSettings _jwtSettings;
+    private readonly IDateProvider _dateProvider;
+
+    public JwtHelper(
+        IOptions<JwtSettings> jwtSettings,
+        IDateProvider dateProvider)
+    {
+        _jwtSettings = jwtSettings.Value;
+        _dateProvider = dateProvider;
+    }
+
+    public string GenerateJwtToken(int userId, UserRole userRole)
+    {
+        var claims = new Claim[]
+        {
+            new Claim(ClaimTypes.Name, userId.ToString()),
+            new Claim(ClaimTypes.Role, userRole.ToString())
+        };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.SecretKey));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+
+        var token = new JwtSecurityToken(
+            _jwtSettings.Issuer,
+            _jwtSettings.Audience,
+            claims,
+            expires: _dateProvider.GetDate().AddMinutes(_jwtSettings.ExpirationMinutes),
+            signingCredentials: creds
+        );
+
+        return tokenHandler.WriteToken(token);
+    }
+}
