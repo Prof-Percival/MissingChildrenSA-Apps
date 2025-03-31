49391d59eae490b4917610e254b6ca6cf1976c89
Prof-Percival <percival.mrd921@yahoo.com>
2025-03-31 09:48:56 +0200

    add TryGetByIdAsync to base repo

--- api/MCSA-API/Data/IRepository.cs
+++ api/MCSA-API/Data/IRepository.cs
@@ -3,6 +3,7 @@
 public interface IRepository<T> where T : class
 {
     Task<T> GetByIdAsync(int id);
+    Task<bool> TryGetByIdAsync(int id, out T entity);
     Task<IEnumerable<T>> GetAllAsync();
     Task AddAsync(T entity);
     Task UpdateAsync(T entity);
