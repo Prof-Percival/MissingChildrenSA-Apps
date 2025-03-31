e1cada0240b9d45a84c381137caa18547b496c84
Prof-Percival <percival.mrd921@yahoo.com>
2025-03-31 09:53:24 +0200

    consolidate add and update methods into single method (Upsert)

--- api/MCSA-API/Data/IRepository.cs
+++ api/MCSA-API/Data/IRepository.cs
@@ -4,7 +4,6 @@ public interface IRepository<T> where T : class
 {
     Task<T> GetByIdAsync(int id);
     Task<IEnumerable<T>> GetAllAsync();
-    Task AddAsync(T entity);
-    Task UpdateAsync(T entity);
+    Task UpsertAsync(T entity);
     Task DeleteAsync(int id);
 }
