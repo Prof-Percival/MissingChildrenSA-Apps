b0ddc2b1998775c1f57cc4a38c1bd835edcd0c24
Prof-Percival <percival.mrd921@yahoo.com>
2025-03-31 20:11:36 +0200

    add create user request and response models

---
+++ api/MCSA-API/Models/Users/CreateUserResponse.cs
@@ -0,0 +1,13 @@
+﻿using MCSA_API.Helpers;
+using MCSA_API.Models.Shared;
+
+namespace MCSA_API.Models.Users;
+
+public sealed class CreateUserResponse : Person
+{
+    public int Id { get; set; }
+    public string Username { get; init; }
+    public string UserRole { get; init; }
+    public string FullName => FormattingHelper.AsFullName(FirstName, LastName);
+    public string Created { get; init; }
+}
