d123ef5b3147aadce36164285f0e8239807e92ff
Prof-Percival <percival.mrd921@yahoo.com>
2025-03-31 15:41:30 +0200

    add auth controller

---
+++ api/MCSA-API/Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+using MCSA_API.Domain.Users;
+using MCSA_API.Models.Auth;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MCSA_API.Controllers;
+
+[ApiVersion("1.0-mcsa")]
+[Route("api/[controller]")]
+public class AuthController(
+    IUserService userService) : BaseApiController
+{
+    [HttpPost("authenticate")]
+    [Produces(typeof(AuthenticateResponse))]
+    public async Task<IActionResult> Authenticate([FromBody] AuthenticateRequest model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest("Username or password is required.");
+        }
+
+        var token = await userService.AuthenticateUserAsync(model.Username, model.Password);
+
+        if (token == null)
+            return Unauthorized("Invalid username or password.");
+
+        return Ok(new AuthenticateResponse
+        {
+            Token = token
+        });
+    }
+}
