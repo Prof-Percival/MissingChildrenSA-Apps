96b52caecc83ff28cd8db5b48513ff398795b3c9
Prof-Percival <percival.mrd921@yahoo.com>
2025-03-31 20:27:36 +0200

    add update user request and response models

---
+++ api/MCSA-API/Models/Users/UpdateUserRequest.cs
@@ -0,0 +1,14 @@
+﻿using MCSA_API.Domain.Users;
+using MCSA_API.Models.Shared;
+using System.ComponentModel.DataAnnotations;
+
+namespace MCSA_API.Models.Users;
+
+public sealed class UpdateUserRequest : Person
+{
+    [Required]
+    [Display(Name = "User Role")]
+    public UserRole? UserRole { get; init; }
+
+    public string Password { get; init; }
+}
