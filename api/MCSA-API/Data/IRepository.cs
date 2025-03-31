301e594f618ee44fe626b5f8d7917f46e5e78832
Prof-Percival <percival.mrd921@yahoo.com>
2025-03-31 09:49:17 +0200

    remove TryGetByIdAsync (CS1988 - You cannot have ref or out parameters in async methods)

--- api/MCSA-API/Data/IRepository.cs
+++ api/MCSA-API/Data/IRepository.cs
@@ -3,7 +3,6 @@
 public interface IRepository<T> where T : class
 {
     Task<T> GetByIdAsync(int id);
-    Task<bool> TryGetByIdAsync(int id, out T entity);
     Task<IEnumerable<T>> GetAllAsync();
     Task AddAsync(T entity);
     Task UpdateAsync(T entity);
