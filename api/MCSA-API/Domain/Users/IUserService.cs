30da11bb6c326813430c1bdb8aba7f8261393bf2
Prof-Percival <percival.mrd921@yahoo.com>
2025-03-31 11:13:00 +0200

    create user service

---
+++ api/MCSA-API/Domain/Users/IUserService.cs
@@ -0,0 +1,7 @@
+﻿namespace MCSA_API.Domain.Users;
+
+public interface IUserService
+{
+    Task<string> AuthenticateUserAsync(string username, string password);
+    Task<User> CreateUserAsync(User user, string password);
+}
---
+++ api/MCSA-API/Domain/Users/UserService.cs
@@ -0,0 +1,41 @@
+﻿using MCSA_API.Domain.Security;
+using Microsoft.AspNetCore.Identity;
+
+namespace MCSA_API.Domain.Users;
+
+public class UserService(
+    IUserRepository userRepository,
+    IJwtHelper jwtHelper) : IUserService
+{
+    private readonly PasswordHasher<User> _passwordHasher = new PasswordHasher<User>();
+
+    public async Task<string> AuthenticateUserAsync(string username, string password)
+    {
+        var user = await userRepository.GetByUsernameAsync(username);
+
+        if (user == null)
+        {
+            return null; // Invalid credentials
+        }
+
+        // Verify password
+        var passwordVerificationResult = _passwordHasher.VerifyHashedPassword(new User(), user.PasswordHash, password);
+
+        if (passwordVerificationResult == PasswordVerificationResult.Failed)
+        {
+            return null; // Invalid password
+        }
+
+        return jwtHelper.GenerateJwtToken(user.Id.Value, user.Role);
+    }
+
+    public async Task<User> CreateUserAsync(User user, string password)
+    {
+        // Hash and Salt the password before saving
+        user.PasswordHash = _passwordHasher.HashPassword(user, password);
+
+        await userRepository.UpsertAsync(user);
+
+        return user;
+    }
+}
