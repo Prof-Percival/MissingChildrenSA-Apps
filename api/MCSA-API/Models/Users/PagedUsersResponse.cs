19324cabac0e458dcd68720485d18f711f07a78d
Prof-Percival <percival.mrd921@yahoo.com>
2025-03-31 21:44:06 +0200

    add get paged users response model

---
+++ api/MCSA-API/Models/Users/PagedUsersResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace MCSA_API.Models.Users;
+
+public sealed class PagedUsersResponse
+{
+    public int TotalCount { get; init; }
+    public int PageNumber { get; init; }
+    public int PageSize { get; init; }
+    public List<GetUserResponse> Users { get; init; }
+}
