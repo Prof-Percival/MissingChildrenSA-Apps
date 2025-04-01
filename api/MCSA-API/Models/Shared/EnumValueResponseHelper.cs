9d29deeb45268638ff95d968c3e6337bc07bf179
Prof-Percival <percival.mrd921@yahoo.com>
2025-04-01 18:08:00 +0200

    add EnumValueResponse model helper to get values

---
+++ api/MCSA-API/Models/Shared/EnumValueResponseHelper.cs
@@ -0,0 +1,16 @@
+﻿using MCSA_API.Helpers;
+
+namespace MCSA_API.Models.Shared;
+
+public static class EnumValueResponseHelper
+{
+    public static List<EnumValueResponse> GetValues<T>() where T : struct, Enum
+    {
+        return EnumHelper.GetValues<T>().Select(enumValue => new EnumValueResponse
+        {
+            Id = Convert.ToInt32(enumValue),
+            Name = enumValue.ToString(),
+            Description = enumValue.GetBestDescription(),
+        }).ToList();
+    }
+}
