f6aa9013ec9d6136f820a1495fd75a417a7f9af5
Prof-Percival <percival.mrd921@yahoo.com>
2025-04-27 17:07:41 +0200

    rename FrmMain to MainForm

--- desktop/MissingChildrenSA/MainForm.Designer.cs
+++ desktop/MissingChildrenSA/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MissingChildrenSA
 {
-    partial class FrmMain
+    partial class MainForm
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             SuspendLayout();
             // 
             // FrmMain
--- desktop/MissingChildrenSA/MainForm.cs
+++ desktop/MissingChildrenSA/MainForm.cs
@@ -1,8 +1,8 @@
 namespace MissingChildrenSA
 {
-    public partial class FrmMain : Form
+    public partial class MainForm : Form
     {
-        public FrmMain()
+        public MainForm()
         {
             InitializeComponent();
         }
