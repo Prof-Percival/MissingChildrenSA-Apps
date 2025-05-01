d2d3d0db81951011beeaaff9a8f7041392427a1c
Prof-Percival <percival.mrd921@yahoo.com>
2025-05-02 01:45:30 +0200

    Create a Custom DelegatingHandler.This will add the Bearer token only if it's available.

---
+++ desktop/MissingChildrenSA/Services/Http/AuthTokenHandler.cs
@@ -0,0 +1,20 @@
+﻿using MissingChildrenSA.Services.Auth;
+
+namespace MissingChildrenSA.Services.Http;
+
+public class AuthTokenHandler : DelegatingHandler
+{
+    protected override async Task<HttpResponseMessage> SendAsync(
+        HttpRequestMessage request,
+        CancellationToken cancellationToken)
+    {
+        var token = AuthService.Token;
+
+        if (!string.IsNullOrEmpty(token))
+        {
+            request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+        }
+
+        return await base.SendAsync(request, cancellationToken);
+    }
+}
