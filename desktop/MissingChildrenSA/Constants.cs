4c8c23df3dfea26fde6428377e6b65ff81a43279
Prof-Percival <percival.mrd921@yahoo.com>
2025-05-05 01:38:37 +0200

    add SuperUser constant

--- desktop/MissingChildrenSA/Constants.cs
+++ desktop/MissingChildrenSA/Constants.cs
@@ -4,4 +4,5 @@ public static class Constants
 {
     public const string ApiBaseUrl = "https://mcsa-backend.azurewebsites.net";
     public const string McsaBaseUrl = "https://missingchildren.org.za";
+    public const string SuperUser = "SuperUser";
 }
