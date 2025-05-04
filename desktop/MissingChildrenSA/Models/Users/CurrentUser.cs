926d4bd3e1d62a6226265e365ebeb42fc5d04759
Prof-Percival <percival.mrd921@yahoo.com>
2025-05-04 18:02:26 +0200

    add CurrentUser model

---
+++ desktop/MissingChildrenSA/Models/Users/CurrentUser.cs
@@ -0,0 +1,11 @@
+﻿namespace MissingChildrenSA.Models.Users;
+
+public sealed class CurrentUser(CurrentUserModel userModel)
+{
+    public string Username { get; } = userModel.Username;
+    public string FirstName { get; } = userModel.FirstName;
+    public string LastName { get; } = userModel.LastName;
+    public string FullName { get; } = userModel.FullName;
+    public string UserRole { get; } = userModel.UserRole;
+    public string Created { get; } = userModel.Created;
+}
