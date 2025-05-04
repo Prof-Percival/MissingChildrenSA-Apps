47b38b95749a60678fab8345f756bd2b7c5e3444
Prof-Percival <percival.mrd921@yahoo.com>
2025-05-04 18:49:18 +0200

    add current user service

---
+++ desktop/MissingChildrenSA/Services/Users/CurrentUserService.cs
@@ -0,0 +1,14 @@
+﻿using MissingChildrenSA.Models.Users;
+
+namespace MissingChildrenSA.Services.Users;
+
+public sealed class CurrentUserService(
+    ApiClient apiClient)
+{
+    public async Task<CurrentUser> GetCurrentUser()
+    {
+        var response = await apiClient.MeAsync();
+
+        return new CurrentUser(response);
+    }
+}
