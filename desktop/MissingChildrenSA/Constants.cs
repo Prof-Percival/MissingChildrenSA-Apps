26b96130a69726f0a45d102751a8fd9a25eef491
Prof-Percival <percival.mrd921@yahoo.com>
2025-05-04 20:23:22 +0200

    Add Mcsa Base url

--- desktop/MissingChildrenSA/Constants.cs
+++ desktop/MissingChildrenSA/Constants.cs
@@ -3,4 +3,5 @@
 public static class Constants
 {
     public const string ApiBaseUrl = "https://mcsa-backend.azurewebsites.net";
+    public const string McsaBaseUrl = "https://missingchildren.org.za";
 }
