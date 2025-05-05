7eb4ea153a66dd6d3012b5773bdb417c4e6505e0
Prof-Percival <percival.mrd921@yahoo.com>
2025-05-05 20:57:37 +0200

    add user model and map fields from api model to app model

---
+++ desktop/MissingChildrenSA/Models/Users/UserModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MissingChildrenSA.Models.Users;
+
+public sealed class UserModel(GetUserResponse user)
+{
+    public int Id { get; } = user.Id;
+    public string Username { get; } = user.Username;
+    public string FirstName { get; } = user.FirstName;
+    public string LastName { get; } = user.LastName;
+    public string FullName { get; } = user.FullName;
+    public string UserRole { get; } = user.UserRole;
+    public string Created { get; } = user.Created;
+    public string Updated { get; } = user.Updated;
+}
