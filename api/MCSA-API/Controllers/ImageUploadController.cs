d006740fc070f2f8d17c1262b538ebd87ae98930
Prof-Percival <percival.mrd921@yahoo.com>
2025-05-07 01:05:12 +0200

    add image controller and endpoint to upload image

---
+++ api/MCSA-API/Controllers/ImageUploadController.cs
@@ -0,0 +1,26 @@
+﻿using MCSA_API.Domain.Azure;
+using MCSA_API.Models.Images;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MCSA_API.Controllers;
+
+[ApiController]
+[ApiVersion("1.0-mcsa")]
+[Route("api/[controller]")]
+public sealed class ImageUploadController(
+    IBlobService blobService) : BaseApiController
+{
+    [HttpPost("upload-image")]
+    [Produces(typeof(ImageMetadataModel))]
+    public async Task<IActionResult> UploadImageAsync([FromForm] IFormFile file)
+    {
+        if (file == null || file.Length == 0)
+        {
+            return BadRequest("No file uploaded.");
+        }
+
+        var metadata = await blobService.UploadImageAsync(file);
+
+        return Ok(new ImageMetadataModel(metadata));
+    }
+}
