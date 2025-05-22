0eb98b63c974f66a4b28240ba6fbbadedac32f7e
Prof-Percival <percival.mrd921@yahoo.com>
2025-05-23 00:44:12 +0200

    add model for moderation stats

---
+++ desktop/MissingChildrenSA/Models/Moderation/ModerationQueueStatsModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MissingChildrenSA.Models.Moderation;
+
+public sealed class ModerationQueueStatsModel(
+    ModerationQueueStatsResponseModel stats)
+{
+    public int Unmoderated { get; } = stats.Unmoderated;
+    public int InModeration { get; } = stats.InModeration;
+    public int Failed { get; } = stats.Failed;
+    public int Approved { get; } = stats.Approved;
+}
