7c5c9b707d72c29d1dfe023ee1b5820be5bb7387
Prof-Percival <percival.mrd921@yahoo.com>
2025-05-25 16:12:35 +0200

    re-design control

--- desktop/MissingChildrenSA/Forms/MissingPersons/MissingPersonTileControl.Designer.cs
+++ desktop/MissingChildrenSA/Forms/MissingPersons/MissingPersonTileControl.Designer.cs
@@ -28,9 +28,78 @@ partial class MissingPersonTileControl
     /// </summary>
     private void InitializeComponent()
     {
-        components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        PicPerson = new PictureBox();
+        LblName = new Label();
+        LblMissingDate = new Label();
+        LblReported = new Label();
+        ((System.ComponentModel.ISupportInitialize)PicPerson).BeginInit();
+        SuspendLayout();
+        // 
+        // PicPerson
+        // 
+        PicPerson.Dock = DockStyle.Top;
+        PicPerson.Location = new Point(0, 0);
+        PicPerson.Name = "PicPerson";
+        PicPerson.Size = new Size(198, 120);
+        PicPerson.SizeMode = PictureBoxSizeMode.Zoom;
+        PicPerson.TabIndex = 3;
+        PicPerson.TabStop = false;
+        PicPerson.Click += Tile_Click;
+        // 
+        // LblName
+        // 
+        LblName.AutoEllipsis = true;
+        LblName.Dock = DockStyle.Top;
+        LblName.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+        LblName.Location = new Point(0, 120);
+        LblName.Name = "LblName";
+        LblName.Size = new Size(198, 30);
+        LblName.TabIndex = 2;
+        LblName.TextAlign = ContentAlignment.MiddleCenter;
+        LblName.Click += Tile_Click;
+        // 
+        // LblMissingDate
+        // 
+        LblMissingDate.Dock = DockStyle.Top;
+        LblMissingDate.Location = new Point(0, 150);
+        LblMissingDate.Name = "LblMissingDate";
+        LblMissingDate.Size = new Size(198, 20);
+        LblMissingDate.TabIndex = 1;
+        LblMissingDate.TextAlign = ContentAlignment.MiddleCenter;
+        LblMissingDate.Click += Tile_Click;
+        // 
+        // LblReported
+        // 
+        LblReported.Dock = DockStyle.Top;
+        LblReported.Location = new Point(0, 170);
+        LblReported.Name = "LblReported";
+        LblReported.Size = new Size(198, 20);
+        LblReported.TabIndex = 0;
+        LblReported.TextAlign = ContentAlignment.MiddleCenter;
+        LblReported.Click += Tile_Click;
+        // 
+        // MissingPersonTileControl
+        // 
+        AutoScaleDimensions = new SizeF(7F, 15F);
+        AutoScaleMode = AutoScaleMode.Font;
+        BackColor = Color.White;
+        BorderStyle = BorderStyle.FixedSingle;
+        Controls.Add(LblReported);
+        Controls.Add(LblMissingDate);
+        Controls.Add(LblName);
+        Controls.Add(PicPerson);
+        Cursor = Cursors.Hand;
+        Margin = new Padding(10, 10, 10, 10);
+        Name = "MissingPersonTileControl";
+        Size = new Size(198, 198);
+        ((System.ComponentModel.ISupportInitialize)PicPerson).EndInit();
+        ResumeLayout(false);
     }
 
     #endregion
+
+    private PictureBox PicPerson;
+    private Label LblName;
+    private Label LblMissingDate;
+    private Label LblReported;
 }
--- desktop/MissingChildrenSA/Forms/MissingPersons/MissingPersonTileControl.cs
+++ desktop/MissingChildrenSA/Forms/MissingPersons/MissingPersonTileControl.cs
@@ -1,18 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using MissingChildrenSA.Helpers.Images;
 
 namespace MissingChildrenSA.Forms.MissingPersons;
+
 public partial class MissingPersonTileControl : UserControl
 {
+    public Models.MissingPersons.MissingPersonModel MissingPerson { get; private set; }
+
+    public event EventHandler TileClicked;
+
     public MissingPersonTileControl()
     {
         InitializeComponent();
     }
+
+    public async void SetData(Models.MissingPersons.MissingPersonModel person)
+    {
+        MissingPerson = person;
+
+        LblName.Text = $"{person.FirstName} {person.LastName}";
+        LblMissingDate.Text = $"Missing: {person.DateWentMissing}";
+        LblReported.Text = $"Reported: {person.Created}";
+
+        await PicPerson.LoadImageFromUrl(person.ImageUrl);
+    }
+
+    private void Tile_Click(object sender, EventArgs e) => TileClicked?.Invoke(this, EventArgs.Empty);
 }
