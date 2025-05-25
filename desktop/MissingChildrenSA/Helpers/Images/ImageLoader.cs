0f609fc34a2939e8f3af6540829f9d993eb040c9
Prof-Percival <percival.mrd921@yahoo.com>
2025-05-25 16:02:52 +0200

    add image loader extension for PictureBox

---
+++ desktop/MissingChildrenSA/Helpers/Images/ImageLoader.cs
@@ -0,0 +1,24 @@
+﻿namespace MissingChildrenSA.Helpers.Images;
+
+public static class ImageLoader
+{
+    public static async Task LoadImageFromUrl(
+        this PictureBox picture,
+        string imageUrl)
+    {
+        try
+        {
+            using var client = new HttpClient();
+
+            var imageBytes = await client.GetByteArrayAsync(imageUrl);
+
+            using var ms = new MemoryStream(imageBytes);
+
+            picture.Image = Image.FromStream(ms);
+        }
+        catch
+        {
+            picture.Image = null;
+        }
+    }
+}
