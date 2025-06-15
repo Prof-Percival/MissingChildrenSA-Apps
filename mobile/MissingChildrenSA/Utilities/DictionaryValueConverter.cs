f2dda39d55002c7c38087f24e3b4a38ca00c426b
Prof-Percival <percival.mrd921@yahoo.com>
2025-06-15 19:54:48 +0200

    add DictionaryValueConverter

---
+++ mobile/MissingChildrenSA/Utilities/DictionaryValueConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+namespace MissingChildrenSA.Utilities;
+
+public class DictionaryValueConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var key = parameter?.ToString();
+
+        if (value is Dictionary<string, string> dict && key != null && dict.TryGetValue(key, out var error))
+        {
+            return error;
+        }
+
+        return null;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
