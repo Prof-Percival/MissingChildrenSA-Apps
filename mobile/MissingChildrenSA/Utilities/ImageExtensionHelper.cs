d08d778a1dce86ddd1bc3821705625427f5c6ff0
Prof-Percival <percival.mrd921@yahoo.com>
2025-06-15 19:45:54 +0200

    add ImageExtensionHelper to help with getting image content type

---
+++ mobile/MissingChildrenSA/Utilities/ImageExtensionHelper.cs
@@ -0,0 +1,18 @@
+﻿namespace MissingChildrenSA.Utilities;
+
+public static class ImageExtensionHelper
+{
+    public static string GetContentType(string imagePath)
+    {
+        var extension = Path.GetExtension(imagePath).ToLowerInvariant();
+
+        return extension switch
+        {
+            ".jpg" or ".jpeg" => "image/jpeg",
+            ".png" => "image/png",
+            ".gif" => "image/gif",
+            ".bmp" => "image/bmp",
+            _ => "application/octet-stream" // fallback for unknown types
+        };
+    }
+}
