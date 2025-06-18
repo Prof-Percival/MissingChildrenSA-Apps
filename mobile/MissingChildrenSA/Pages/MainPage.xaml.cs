08a15dae38dd5dc60341fd40ed3902f71d70d83a
Prof-Percival <percival.mrd921@yahoo.com>
2025-06-18 10:41:35 +0200

    remove testing logic for the mainpage stats

--- mobile/MissingChildrenSA/Pages/MainPage.xaml.cs
+++ mobile/MissingChildrenSA/Pages/MainPage.xaml.cs
@@ -2,42 +2,10 @@
 {
     public partial class MainPage : ContentPage
     {
-        private readonly ApiClient _apiClient;
-
-        public MainPage(MainPageModel model, ApiClient apiClient)
+        public MainPage(MainPageModel model)
         {
             InitializeComponent();
             BindingContext = model;
-            _apiClient = apiClient;
-
-            //call FetchDashboardStats on page load
-            Appearing += async (s, e) =>
-            {
-                await FetchDashboardStats();
-            };
-        }
-
-        private async Task FetchDashboardStats()
-        {
-            try
-            {
-                var stats = await _apiClient.GetMissingPersonsStatisticsAsync();
-
-                if (stats != null)
-                {
-                    throw new Exception();
-                    // Update the UI with the fetched stats
-                    // For example, you can bind these stats to labels or charts in your XAML
-                }
-            }
-            catch (Exception ex)
-            {
-                await AppShell.DisplaySnackbarAsync($"Error fetching dashboard stats: {ex.Message}");
-
-                await Task.Delay(3000);
-
-                await AppShell.DisplayToastAsync($"Error fetching dashboard stats: {ex.Message}");
-            }
         }
     }
 }
