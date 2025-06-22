99fcc70fa570eda74235df4e6b52bb54e8cdb40d
Prof-Percival <percival.mrd921@yahoo.com>
2025-06-22 19:02:29 +0200

    add display names and required attributes to other fields

--- api/MCSA-API/Models/MissingPersons/CreateMissingPersonRequest.cs
+++ api/MCSA-API/Models/MissingPersons/CreateMissingPersonRequest.cs
@@ -20,15 +20,19 @@ public class CreateMissingPersonRequest : Person
     public Province? Province { get; set; }
 
     [Required]
+    [Display(Name = "Date Went Missing")]
     public DateTimeOffset? DateWentMissing { get; set; }
 
     [Required]
+    [Display(Name = "Hair Colour")]
     public string HairColour { get; set; }
 
     [Required]
+    [Display(Name = "Hair Length Or Style")]
     public string HairLengthOrStyle { get; set; }
 
     [Required]
+    [Display(Name = "Eye Colour")]
     public string EyeColour { get; set; }
 
     [Required]
@@ -37,25 +41,43 @@ public class CreateMissingPersonRequest : Person
     [Required]
     public double? Weight { get; set; }
 
+    [Required]
+    [Display(Name = "Marks / Tattoes")]
     public string MarksTattoos { get; set; }
+
+    [Required]
+    [Display(Name = "Last Seen Wearing")]
     public string LastSeenWearing { get; set; }
+
+    [Required]
+    [Display(Name = "Location Last Seen")]
     public string LocationLastSeen { get; set; }
 
     [Required]
+    [Display(Name = "Image Url")]
     public string ImageUrl { get; set; }
 
     [Required]
+    [Display(Name = "Police Station")]
     public string PoliceStation { get; set; }
 
     [Required]
+    [Display(Name = "Case Number")]
     public string CaseNumber { get; set; }
 
     [Required]
+    [Display(Name = "Investigating Officer")]
     public string InvestigatingOfficer { get; set; }
 
     [Required]
+    [Display(Name = "Investigating Officer Contact Number")]
     public string InvestigatingOfficerContactNumber { get; set; }
 
+    [Required]
+    [Display(Name = "Family Contact Name")]
     public string FamilyContactName { get; set; }
+
+    [Required]
+    [Display(Name = "Family Contact Number")]
     public string FamilyContactNumber { get; set; }
 }
