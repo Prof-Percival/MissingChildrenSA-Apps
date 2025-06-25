db86cb4034e44481b0ec7515bfe8629b1332ec7f
Prof-Percival <percival.mrd921@yahoo.com>
2025-06-26 00:38:29 +0200

    JWT helper tests

---
+++ api/MCSA_API.Tests/Helpers/JwtHelperTests.cs
@@ -0,0 +1,48 @@
+﻿using MCSA_API.Domain.Dates;
+using MCSA_API.Domain.Security;
+using MCSA_API.Domain.Users;
+using Microsoft.Extensions.Options;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace MCSA_API.Tests.Helpers;
+
+[TestClass]
+public class JwtHelperTests
+{
+    private JwtHelper _jwtHelper;
+
+    [TestInitialize]
+    public void Init()
+    {
+        var jwtSettings = new JwtSettings
+        {
+            SecretKey = "SuperSecretKey123456789012345678",
+            Issuer = "MCSA",
+            Audience = "MCSAUsers",
+            ExpirationMinutes = 60
+        };
+
+        var dateProvider = new DateProvider();
+
+        _jwtHelper = new JwtHelper(Options.Create(jwtSettings), dateProvider);
+    }
+
+    [TestMethod]
+    public void GenerateJwtToken_ValidInput_ReturnsToken()
+    {
+        var handler = new JwtSecurityTokenHandler();
+
+        var (token, role, expiry) = _jwtHelper.GenerateJwtToken(1, UserRole.SuperUser);
+
+        Assert.IsFalse(string.IsNullOrWhiteSpace(token));
+
+        var jwt = handler.ReadJwtToken(token);
+
+        var nameClaim = jwt.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Name);
+        var roleClaim = jwt.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Role);
+
+        Assert.AreEqual("1", nameClaim?.Value);
+        Assert.AreEqual("SuperUser", roleClaim?.Value);
+    }
+}
