8268b0fe21986011732e49fbd7adadcdb8371dd8
Prof-Percival <percival.mrd921@yahoo.com>
2025-06-26 00:34:40 +0200

    test client factory

---
+++ api/MCSA_API.Tests/TestUtils/TestClientFactory.cs
@@ -0,0 +1,12 @@
+﻿namespace MCSA_API.Tests.TestUtils;
+
+public static class TestClientFactory
+{
+    public static HttpClient CreateClient()
+    {
+        return new HttpClient
+        {
+            BaseAddress = new Uri("https://mcsa-backend.azurewebsites.net")
+        };
+    }
+}
